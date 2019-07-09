17f1d25ff0dbbcc62fa140323a6c0065483be11e
Krzysztof Krysiak <krysiak86@gmail.com>
2019-07-09 22:28:54 +0200

    Add logger example (EventEmitter)

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // const Person = require('./person');
-import Person from './person';
+// import Person from './person';
 
-const person1 = new Person("Holly", 33);
-person1.greeting();
+// const person1 = new Person("Holly", 33);
+// person1.greeting();
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('Hello World!');
