0387f9f5877f0f93975f9001ad287f2d80836d36
Krzysztof Krysiak <krysiak86@gmail.com>
2019-07-09 22:14:24 +0200

    Add more params examples for url_demo.js

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -11,3 +11,11 @@ console.log(myUrl.host);
 console.log(myUrl.hostname);
 // Pathname
 console.log(myUrl.pathname);
+// Serialized query
+console.log(myUrl.search);
+// Params object
+console.log(myUrl.searchParams);
+myUrl.searchParams.append('abc', '123');
+console.log(myUrl.searchParams);
+// Loop through params
+myUrl.searchParams.forEach((value, name) => console.log(`${name}: ${value}`));
